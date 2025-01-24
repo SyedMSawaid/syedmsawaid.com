83a60d2a438fc885ddcf4f16f4c0eb30cbafbf58
Syed M. Sawaid <34231494+SyedMSawaid@users.noreply.github.com>
2025-01-24 15:07:50 +0100

    added layout to chapters at build time

--- plugins/builders/books.rb
+++ plugins/builders/books.rb
@@ -2,19 +2,29 @@
 class Builders::Books < SiteBuilder
   def build
     generator do
-      book_ids = site.collections.books.resources.map {
-        |b| b.id.split("_books/").last.split("/").first
-      }.uniq
+      generate_page_for_books
+      add_layout_to_book_chapters
+    end
+  end
 
+  def add_layout_to_book_chapters
+    site.collections.books.resources.each do |chapter|
+      chapter.data.layout = "default"
+    end
+  end
 
-      book_ids.each do |book|
-        content = create_chapters_list_for(book)
-        add_resource :book, "books/#{book}.md" do
-          layout :default
-          title book.titlecase
-          permalink "/books/:slug/"
-          content content
-        end
+  def generate_page_for_books
+    book_ids = site.collections.books.resources.map {
+      |b| b.id.split("_books/").last.split("/").first
+    }.uniq
+
+    book_ids.each do |book|
+      content = create_chapters_list_for(book)
+      add_resource :book, "books/#{book}.md" do
+        layout :default
+        title book.titlecase
+        permalink "/books/:slug/"
+        content content
       end
     end
   end
